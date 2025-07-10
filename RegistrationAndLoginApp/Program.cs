3ab467c2d28e5a3ba89e8efceb66c8f46c0453be
EsraaAlzoghby <esraa.m.zogpy@gmail.com>
2025-07-10 09:36:15 +0300

    Add project files.

---
+++ RegistrationAndLoginApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RegistrationAndLoginApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Registration 
+            Console.WriteLine("====== User Registration ======");
+            Console.Write("Enter a username: ");
+            string? username = Console.ReadLine();
+            Console.Write("Enter a password: ");
+            string? password = Console.ReadLine();
+            Console.WriteLine("\nRegistration completed successfully!");
+            Console.WriteLine("Press any key to continue to login...");
+            Console.ReadKey();
+            //clear
+            Console.Clear();
+            //Login
+            Console.WriteLine("====== User Login ======");
+            Console.Write("Enter your username: ");
+            string? loginUsername = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            string? loginPassword = Console.ReadLine();
+
+            if (username == loginUsername && password == loginPassword)
+            {
+                Console.WriteLine($"Login successful! Welcome,{loginUsername}");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Invalid username or password.");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
